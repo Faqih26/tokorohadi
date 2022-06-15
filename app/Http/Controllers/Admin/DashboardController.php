8812cd3535d8a5d216fde04e220a626f9b57eec3
faqihsuryana <faqihsuryana148@gmail.com>
2022-06-15 16:34:09 +0700

    first commit

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\User;
+use App\Transaction;
+use App\Models\TransactionDetail;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $customer = User::count();
+        $revenue = Transaction::where('transaction_status','SUCCESS')->sum('total_price');
+        $transaction = Transaction::count();
+
+        $transactions = TransactionDetail::with(['transaction.user','product.galleries'])
+                        ->whereHas('product',function($product){
+                            $product->where('users_id',Auth::user()->id);
+                        });
+
+        return view('pages.admin.dashboard',[
+            'customer' => $customer,
+            'revenue' => $revenue,
+            'transaction' => $transaction,
+            'transaction_data' => $transactions->get(),
+        ]);
+    }
+
+}
---
+++ resources/views/pages/faq.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title')
+  Kontak Kami
+@endsection
+
+@section('content')
+        <div class="page-content page-home" >
+            <h5 style="text-align: center">HUBUNGI KAMI JIKA ADA HAL YANG INGIN DITANYAKAN</h5>
+                <h6 style="text-align: center">No WhatsApp :
+                    <a href="https://wa.me/6285797168036?text=contoh%20isi%20pesan%20dikirim%20via%20whatsapp" style="text-align: center"><img src="/images/kontak.jpg" style="height:44px;width:auto;"></a>
+                </h6>
+            <H6 style="text-align: center">
+                EMAIL   : faqihsuryana148@gmail.com
+            </H6>
+            <H6 style="text-align: center">
+                Alamat  : Pasar Pulogadung Jakarta Timur
+            </H6>
+        </div>
+@endsection
